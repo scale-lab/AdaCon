6901931b2ff7d5eaada1386f1c9f28342fa2c5e3
MarinaHesham <marinahesham21@gmail.com>
2021-01-25 11:49:23 -0500

    add a file to download the weights instead on having them in the initial repo

---
+++ weights/download_adacon_weights.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Get yolov3 weights (used as backbone)
+wget -c https://pjreddie.com/media/files/yolov3.weights -P weights
+# wget -c https://pjreddie.com/media/files/yolov3-tiny.weights
+# wget -c https://pjreddie.com/media/files/yolov3-spp.weights
+
+# Get AdaCon branch weights 
+wget -c http://scale.engin.brown.edu/tools/AdaCon/adacon_branch0v0.pt -P weights
+wget -c http://scale.engin.brown.edu/tools/AdaCon/adacon_branch1v0.pt -P weights
+wget -c http://scale.engin.brown.edu/tools/AdaCon/adacon_branch2v0.pt -P weights
+wget -c http://scale.engin.brown.edu/tools/AdaCon/adacon_branch3v0.pt -P weights
+
+# Get AdaCon Branch Controller
+wget -c http://scale.engin.brown.edu/tools/AdaCon/adacon_branch_controller.pth -P weights
