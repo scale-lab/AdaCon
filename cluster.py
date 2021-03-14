44f15fce82b49dcef55e883279ad1dc959e9824d
MarinaHesham <marinahesham21@gmail.com>
2021-03-14 16:36:48 -0400

    restructure cluster.py

--- cluster.py
+++ cluster.py
@@ -15,7 +15,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import networkx as nx
-from sklearn.cluster import KMeans,AgglomerativeClustering
+from sklearn.cluster import AgglomerativeClustering
 from matplotlib.pylab import show, cm, axis
 from matplotlib.colors import LinearSegmentedColormap
 
@@ -23,14 +23,87 @@ seed = 578912
 random.seed(seed)
 np.random.seed(seed)
 
-def kmeans_clustering(classes_positions, num_of_clusters):
-    keys = sorted([k for k in classes_positions.keys()])
+def _get_adjacency_matrix(train_path, num_classes, co_occurance_out_file):
+    if os.path.exists(co_occurance_out_file):
+        objects_adjacency_matrix = np.genfromtxt(co_occurance_out_file, delimiter=',', dtype=float)
+    else:
+        dir_name = os.path.dirname(train_path)
+        print("Extracting the co-occurance matrix from the training dataset")
+        objects_adjacency_matrix = np.zeros((num_classes,num_classes))
+
+        # Loop on Images Labels files
+        f = open(train_path, "r")
+        train_paths = f.readlines()
+        train_paths = [
+            os.path.join(dir_name, path.rstrip().replace("./", ""). \
+                replace("images", "labels").replace(".png", ".txt").replace(".jpg", ".txt"))
+            for path in train_paths
+        ]
+
+        for filename in train_paths:
+            if filename.endswith(".txt"):
+                # Loop on Objects inside each Image
+                objects = []
+                with open(filename, "r") as a_file:
+                    for line in a_file:
+                        stripped_line = line.split(" ")
+                        objects.append(int(stripped_line[0]))
+                for obj1 in objects:
+                    for obj2 in objects:
+                        objects_adjacency_matrix[obj1,obj2] += 1
+                        objects_adjacency_matrix[obj2,obj1] += 1
+
+        np.savetxt(co_occurance_out_file, objects_adjacency_matrix, delimiter=",", fmt='%d')
+
+    return objects_adjacency_matrix
+
+def _get_common_classes(adjacency_matrix):
+    temp = np.copy(adjacency_matrix)
+
+    temp[temp<100] = 0
+    temp[temp>=100] = 1
+
+    common_classes = []
+    for i in range(len(temp)):
+        if np.sum(temp[i]) >= opt.common_classes:
+            common_classes.append(i)
+    return common_classes
+
+def _remove_common_classes(objects_adjacency_matrix, common_classes, num_classes):
+    # Get mapping to classes idx after removing common classes
+    classes_idx_dict = {}
+    j = 0
+    for i in range(num_classes):
+        if i not in common_classes:
+            classes_idx_dict[j] = i
+            j += 1
+
+    # Remove common classes
+    r_objects_matrix = [[objects_adjacency_matrix[i,j] for j in range(len(objects_adjacency_matrix)) if j not in common_classes] 
+                       for i in range(len(objects_adjacency_matrix)) if i not in common_classes]
+
+
+    r_objects_matrix = np.asarray(r_objects_matrix)
+    r_objects_matrix = [[r_objects_matrix[i,j] if i != j else 0 for i in range(len(r_objects_matrix))] for j in range(len(r_objects_matrix)) ]
+
+    r_objects_matrix = np.asarray(r_objects_matrix)
+
+    return classes_idx_dict, r_objects_matrix
+
+def _get_probability_adjacency_matrix(adjacency_matrix):
+    prob = np.copy(adjacency_matrix)
+    for i in range(len(adjacency_matrix)):
+        for j in range(len(adjacency_matrix)):
+            prob[i,j] = adjacency_matrix[i,j]/(np.sum(adjacency_matrix[:,j])+1)
+    return prob
+
+def _cluster(positions, num_of_clusters):
+    keys = sorted([k for k in positions.keys()])
     classes_dict = {}
-    for i, key in enumerate(classes_positions.keys()):
+    for i, key in enumerate(positions.keys()):
         classes_dict[i] = key
 
-    positions = [p for p in classes_positions.values()]
-    #kmeans = KMeans(n_clusters=num_of_clusters, random_state=0,n_init=100).fit(positions)
+    positions = [p for p in positions.values()]
     kmeans = AgglomerativeClustering(n_clusters=num_of_clusters).fit(positions)
     labels = kmeans.labels_
     clusters = []
@@ -38,7 +111,35 @@ def kmeans_clustering(classes_positions, num_of_clusters):
         clusters.append([classes_dict[i] for i in range(len(labels)) if labels[i] == c])
     return clusters
 
-def evaluate_clustering(valid_path, common_classes, clusters_list):
+def _create_graph(adjacency_matrix, pruning_threshold, itterations):
+    ''' create_graph takes an adjacency matrix and the pruning threshold
+        It returns the positions of the nodes in the graph,
+        and the adjacency matrix in sparse format
+    '''
+    G = nx.Graph()
+    # Add classes names as graph nodes
+    for i in range(len(adjacency_matrix)):
+        G.add_node(i)
+
+    # Add the a weighed edge between the nodes if 
+    # the corresponding value in the adjacency matrix 
+    # is greater than the pruning threshold
+    for i, row in enumerate(adjacency_matrix):
+        for j, w in enumerate(row):
+            if i == j:
+                continue
+            if adjacency_matrix[i,j] > pruning_threshold:
+                G.add_edge(i,j,weight=w)
+
+    # Convert the Graph to a sparse format matrix to use in plotting
+    s_matrix = nx.to_scipy_sparse_matrix(G)
+
+    # Spead the graph according to the weights to get the nodes positions
+    n_positions = nx.spring_layout(G, iterations=itterations)
+    
+    return n_positions, s_matrix
+
+def _evaluate_clustering(valid_path, common_classes, clusters_list):
     dataset = LoadImagesAndLabels(valid_path, 416, 8, rect=False, single_cls=False, pad=0.5)
     dloader = DataLoader(dataset,
                         batch_size=8,
@@ -66,20 +167,26 @@ def evaluate_clustering(valid_path, common_classes, clusters_list):
 
     return neglected_objects, all_objects
 
-def create_graph(matrix,pruning_threshold):
-    G = nx.Graph()
-    for i in range(len(matrix)):
-        G.add_node(i)
-    for i, row in enumerate(matrix):
-        for j, w in enumerate(row):
-            if i == j:
-                continue
-            if matrix[i,j] > pruning_threshold:
-                G.add_edge(i,j,weight=w)
+def _plot_graph(s_adjmatrix, positions, clusters, classes_ids, common_classes, classes_names, fig_name):
+    fig = plt.figure()
+    graph = nx.Graph(s_adjmatrix)
 
-    mat = nx.to_scipy_sparse_matrix(G)
+    # map node to cluster id for colors
+    cluster_map = {node: i for i, cluster in enumerate(clusters) for node in cluster}
+    colors = [cluster_map[i] for i in range(len(graph.nodes()))]
     
-    return G, mat
+    # retrieve the node labels (class names)
+    labels = {}
+    j = 0
+    for i in range(len(classes_ids)):
+        if i not in common_classes:
+            labels[j] = classes_names[i]
+            j += 1
+
+    nx.draw_networkx_nodes(graph,  pos=positions, node_size=700, node_color=colors,cmap=cm.cool)
+    nx.draw_networkx_labels(graph,  pos=positions, labels=labels, font_size=12)
+
+    plt.savefig(fig_name)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -88,7 +195,7 @@ if __name__ == "__main__":
     parser.add_argument("--output", type=str, default="cluster_gen.data", help="Path to output the clusters")
     parser.add_argument("--data", type=str, default="data/coco2014.data", help="path to data config file")
     parser.add_argument("--pruning_threshold", type=float, default=0.01, help="Pruning threshold for edge weights")
-    parser.add_argument("--cooccurance", type=str, default="co-occurence_adjacency_matrix.csv", help="Use if you already have the co-occurance matrix from previous runs")
+    parser.add_argument("--co-occurance_out", type=str, default="co-occurence_adjacency_matrix.csv", help="Use if you already have the co-occurance matrix from previous runs")
 
     opt = parser.parse_args()
 
@@ -102,118 +209,42 @@ if __name__ == "__main__":
     dir_name = os.path.dirname(train_path)
 
     ##### Compute the frequency based adjacency Matrix #####
-    if not os.path.exists(opt.cooccurance):
-        print("Extracting the co-occurance matrix from the training dataset")
-        objects_adjacency_matrix = np.zeros((num_classes,num_classes))
-
-        # Loop on Images Labels files
-        f=open(train_path, "r")
-        train_paths = f.readlines()
-        train_paths = [
-            os.path.join(dir_name, path.rstrip().replace("./", ""). \
-                replace("images", "labels").replace(".png", ".txt").replace(".jpg", ".txt"))
-            for path in train_paths
-        ]
-
-        for filename in train_paths:
-            if filename.endswith(".txt"):
-                # Loop on Objects inside each Image
-                objects = []
-                with open(filename, "r") as a_file:
-                    for line in a_file:
-                        stripped_line = line.split(" ")
-                        objects.append(int(stripped_line[0]))
-                for obj1 in objects:
-                    for obj2 in objects:
-                        objects_adjacency_matrix[obj1,obj2] += 1
-                        objects_adjacency_matrix[obj2,obj1] += 1
-
-        np.savetxt("co-occurence_adjacency_matrix.csv", objects_adjacency_matrix, delimiter=",", fmt='%d')
-
-    else:
-        objects_adjacency_matrix = np.genfromtxt(opt.cooccurance, delimiter=',',dtype=float)
+    adjacency_matrix = _get_adjacency_matrix(train_path=train_path, 
+                                            num_classes=num_classes, 
+                                            co_occurance_out_file=opt.co_occurance_out)
 
     # chosen_classes = [0, 2,3, 5, 7,9,12, 18, 17, 19, 26,  41, 42, 43, 44, 45, 48, 57, 63, 62, 64,65,66,68,69, 71, 73]
     chosen_classes = np.arange(80)
     classes_names = np.asarray(classes_names)
-    classes_names = classes_names[chosen_classes]
 
-    #### Cluster the objects according to the adjacency matrix #####
-    objects_adjacency_matrix = objects_adjacency_matrix[chosen_classes,:]
-    objects_adjacency_matrix = objects_adjacency_matrix[:,chosen_classes]
+    if len(chosen_classes) < num_classes:
+        classes_names = classes_names[chosen_classes]
+        adjacency_matrix = adjacency_matrix[chosen_classes,:]
+        adjacency_matrix = adjacency_matrix[:,chosen_classes]
 
     # Get Common Classes
-    temp = np.copy(objects_adjacency_matrix)
+    common_classes = _get_common_classes(adjacency_matrix)
+    classes_idx_dict, adjacency_matrix = _remove_common_classes(adjacency_matrix, 
+                                                                common_classes=common_classes,
+                                                                num_classes=num_classes)
 
-    temp[temp<100] = 0
-    temp[temp>=100] = 1
-
-    common_count = 0
-    common_classes = []
-    for i in range(len(temp)):
-        if np.sum(temp[i]) >= opt.common_classes:
-            common_classes.append(i)
-            common_count += 1
-
-    # Get mapping to classes idx after removing common classes
-    classes_idx_dict = {}
-    j = 0
-    for i in range(num_classes):
-        if i not in common_classes:
-            classes_idx_dict[j] = i
-            j += 1
-
-    # Remove common classes
-    r_objects_matrix = [[objects_adjacency_matrix[i,j] for j in range(len(objects_adjacency_matrix)) if j not in common_classes] 
-                       for i in range(len(objects_adjacency_matrix)) if i not in common_classes]
-
-
-    r_objects_matrix = np.asarray(r_objects_matrix)
-    r_objects_matrix = [[r_objects_matrix[i,j] if i != j else 0 for i in range(len(r_objects_matrix))] for j in range(len(r_objects_matrix)) ]
-
-    r_objects_matrix = np.asarray(r_objects_matrix)
-
-    # Get the probabilities for the adjacency matrix
-    prob = np.copy(r_objects_matrix)
-    for i in range(len(r_objects_matrix)):
-        for j in range(len(r_objects_matrix)):
-            prob[i,j] = r_objects_matrix[i,j]/(np.sum(r_objects_matrix[:,j])+1)
-
-    ## Draw Graph and MCL Clustering results
-    labels = {}
-    j = 0
-    for i in range(len(chosen_classes)):
-        if i not in common_classes:
-            labels[j] = classes_names[i]
-            j += 1
+    # Convert frequency based agjacency matrix to probablility based adjacency matrix
+    prob_adjacency_matrix = _get_probability_adjacency_matrix(adjacency_matrix)
             
-    G, mat = create_graph(prob,0.005)
-    pos=nx.spring_layout(G,iterations=40)
+    # Convert the probability based adjacency matrix to a graph
+    n_positions, s_matrix = _create_graph(prob_adjacency_matrix, pruning_threshold=opt.pruning_threshold, itterations=40)
 
-    clusters = kmeans_clustering(pos, num_clusters)
+    # Cluster the classes based on their positions in the graph
+    clusters = _cluster(n_positions, num_clusters)
 
-    # make a networkx graph from the adjacency matrix
-    fig = plt.figure()
-    graph = nx.Graph(mat)
-
-    # map node to cluster id for colors
-    cluster_map = {node: i for i, cluster in enumerate(clusters) for node in cluster}
-    colors = [cluster_map[i] for i in range(len(graph.nodes()))]
-    
-    # draw
-    nx.draw_networkx_nodes(graph,  pos=pos, node_size=700, node_color=colors,cmap=cm.cool)
-    nx.draw_networkx_labels(graph,  pos=pos, labels=labels, font_size=12)
-
+    # Plot the graph with the clustered classes
     fig_name = "cluster_" + str(opt.num_clusters) + "_" + str(opt.common_classes) + ".png"
-    plt.savefig(fig_name)
+    _plot_graph(s_matrix, n_positions, clusters, chosen_classes, common_classes, classes_names, fig_name)
 
-    ## Add common classes to clusters list
+    ## Add common classes to clusters list and save the clusters file
     for idx, cluster in enumerate(clusters):
         clusters[idx] = [classes_idx_dict[i] for i in cluster]
         clusters[idx].extend(common_classes)
-    
-    for c in clusters:
-        print(len(c), classes_names[c])
     
     with open(opt.output, 'w') as f:
         for cluster in clusters:
@@ -222,7 +253,7 @@ if __name__ == "__main__":
             f.write("\n")
 
     ## Evaluate the clustering by checking the branch miss rate
-    neglected, all_obj = evaluate_clustering(valid_path, common_classes,clusters)
+    neglected, all_obj = _evaluate_clustering(valid_path, common_classes, clusters)
     print(neglected/all_obj*100, "%")
 
 
