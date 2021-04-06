79227f631b5148573907b7791d07ad3ad89dd335
MarinaHesham <marinahesham21@gmail.com>
2021-04-06 18:39:47 -0400

    ...

--- retinanet/test.py
+++ retinanet/test.py
@@ -128,23 +128,14 @@ def main(args):
     # Data loading code
     print("Loading data")
 
-    dataset, num_classes = get_dataset(args.dataset, "train", get_transform(train=True), args.data_path)
-    dataset_test, _ = get_dataset(args.dataset, "val", get_transform(train=False), args.data_path)
+    dataset_test, num_classes = get_dataset(args.dataset, "val", get_transform(train=False), args.data_path)
 
     print("Creating data loaders")
-    train_sampler = torch.utils.data.RandomSampler(dataset)
     test_sampler = torch.utils.data.SequentialSampler(dataset_test)
-
-    if args.aspect_ratio_group_factor >= 0:
-        group_ids = create_aspect_ratio_groups(dataset, k=args.aspect_ratio_group_factor)
-        train_batch_sampler = GroupedBatchSampler(train_sampler, group_ids, args.batch_size)
-    else:
-        train_batch_sampler = torch.utils.data.BatchSampler(
-            train_sampler, args.batch_size, drop_last=True)
 
     data_loader_test = torch.utils.data.DataLoader(
         dataset_test, batch_size=args.batch_size,
-        sampler=test_sampler, num_workers=args.workers,
+        sampler=test_sampler, num_workers=0,
         collate_fn=utils.collate_fn)
 
     print("Creating model")
@@ -155,22 +146,19 @@ def main(args):
         active_branch = args.active_branch
         if args.model == "retinanet":
             model = adacon_retinanet_resnet50_fpn(clusters=clusters, active_branch=active_branch, num_branches=len(clusters),
-                                num_classes=num_classes, trainable_backbone_layers=args.trainable_backbone_layers,
+                                num_classes=num_classes, trainable_backbone_layers=0,
                                 pretrained=args.pretrained, pretrained_backbone=args.pretrained_backbone, branches_weights=args.branches,
                                 backbone_weights=args.backbone_weights, bc_weights=args.branch_controller,
-                                min_size=min_size, max_size=max_size, ckpt=args.resume, deploy=args.deploy)
-            freeze_adacon_retinanet_all_non_active_layers(model, active_branch, args.enable_branch_controller)
+                                min_size=min_size, max_size=max_size, deploy=args.deploy)
+            freeze_adacon_retinanet_all_non_active_layers(model, active_branch, False)
 
         elif args.model == "rcnn":
             model = adacon_fasterrcnn_mobilenet_v3_large_320_fpn(clusters=clusters, active_branch=active_branch, num_branches=len(clusters),
-                                num_classes=num_classes, trainable_backbone_layers=args.trainable_backbone_layers,
+                                num_classes=num_classes, trainable_backbone_layers=0,
                                 pretrained=args.pretrained, pretrained_backbone=args.pretrained_backbone, branches_weights=args.branches,
                                 backbone_weights=args.backbone_weights, bc_weights=args.branch_controller,
-                                min_size=min_size, max_size=max_size, ckpt=args.resume)
-            freeze_adacon_rcnn_all_non_active_layers(model, active_branch, args.enable_branch_controller)
-
-        if args.enable_branch_controller:
-            model.enable_branch_controller = True
+                                min_size=min_size, max_size=max_size)
+            freeze_adacon_rcnn_all_non_active_layers(model, active_branch, False)
 
         if args.oracle:
             model.oracle = True
@@ -180,10 +168,10 @@ def main(args):
         model.to(device)
     else:
         if args.model == "retinanet":
-            model = retinanet_resnet50_fpn(num_classes=num_classes, trainable_backbone_layers=args.trainable_backbone_layers,
+            model = retinanet_resnet50_fpn(num_classes=num_classes, trainable_backbone_layers=0,
                             pretrained=args.pretrained, pretrained_backbone=args.pretrained_backbone, 
                             backbone_weights="retinanet_coco_backbone.pt", head_weights="retinanet_coco_head.pt",
-                            min_size=min_size, max_size=max_size, ckpt=args.resume)
+                            min_size=min_size, max_size=max_size)
         
         elif args.model == "rcnn":
             model = fasterrcnn_mobilenet_v3_large_320_fpn(pretrained=args.pretrained, min_size=min_size, max_size=max_size)
@@ -207,29 +195,8 @@ if __name__ == "__main__":
     parser.add_argument('--device', default='cuda', help='device')
     parser.add_argument('-b', '--batch-size', default=2, type=int,
                         help='images per gpu, the total batch size is $NGPU x batch_size')
-    parser.add_argument('--epochs', default=26, type=int, metavar='N',
-                        help='number of total epochs to run')
-    parser.add_argument('-j', '--workers', default=0, type=int, metavar='N',
-                        help='number of data loading workers (default: 4)')
-    parser.add_argument('--lr', default=0.02, type=float,
-                        help='initial learning rate, 0.02 is the default value for training '
-                        'on 8 gpus and 2 images_per_gpu')
-    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                        help='momentum')
-    parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
-                        metavar='W', help='weight decay (default: 1e-4)',
-                        dest='weight_decay')
-    parser.add_argument('--lr-step-size', default=8, type=int, help='decrease lr every step-size epochs')
-    parser.add_argument('--lr-steps', default=[16, 22], nargs='+', type=int, help='decrease lr every step-size epochs')
-    parser.add_argument('--lr-gamma', default=0.1, type=float, help='decrease lr by a factor of lr-gamma')
-    parser.add_argument('--print-freq', default=20, type=int, help='print frequency')
-    parser.add_argument('--output-dir', default='.', help='path where to save')
-    parser.add_argument('--resume', default='', help='resume from checkpoint')
     parser.add_argument('--backbone-weights', dest="backbone_weights", type=str, default='backbone_coco.pth', help='load backbone weights')
     parser.add_argument('--deploy', dest="deploy", type=str, help='combined weights for deployment')
-    parser.add_argument('--start_epoch', default=0, type=int, help='start epoch')
-    parser.add_argument('--aspect-ratio-group-factor', default=3, type=int)
-    parser.add_argument('--rpn-score-thresh', default=None, type=float, help='rpn score threshold for faster-rcnn')
     parser.add_argument('--trainable-backbone-layers', default=None, type=int,
                         help='number of trainable layers of backbone')
     parser.add_argument(
@@ -278,16 +245,6 @@ if __name__ == "__main__":
     parser.add_argument('--branches', nargs='+', help='trained branches for adaptive test', required=False)
     parser.add_argument('--branch_controller', type=str, help='trained branch controller for adaptive test', required=False)
 
-    parser.add_argument('--enable-branch-controller', dest="enable_branch_controller", action="store_true",
-                        help='Train Branch Controller')
-    # distributed training parameters
-    parser.add_argument('--world-size', default=1, type=int,
-                        help='number of distributed processes')
-    parser.add_argument('--dist-url', default='env://', help='url used to set up distributed training')
-
     args = parser.parse_args()
-
-    if args.output_dir:
-        utils.mkdir(args.output_dir)
 
     main(args)
